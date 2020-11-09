93e1a5a97ebd7a40c473dc723a80d593151834f2
huanf921 <554396016@qq.com>
2020-11-09 17:00:43 +0800

    创建51Job爬虫项目基本包

---
+++ src/main/java/com/vs/wuyijobrecruitspider/WuyijobrecruitspiderApplication.java
@@ -0,0 +1,13 @@
+package com.vs.wuyijobrecruitspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuyijobrecruitspiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuyijobrecruitspiderApplication.class, args);
+    }
+
+}
